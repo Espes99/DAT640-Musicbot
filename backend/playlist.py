58b8e4942ad95e113367d6ded513ab3df2cf3d66
DESKTOP-C43IAH4\Espen <espesele@stud.ntnu.no>
2024-10-03 16:56:22 +0200

    Working communication between bot and backend

---
+++ backend/playlist.py
@@ -0,0 +1,33 @@
+class Playlist:
+    def __init__(self, name, songs):
+        self.name = name
+        self.songs = songs
+
+    def add_song(self, song):
+        song = song.lower()
+        if song not in self.songs:
+            self.songs.append(song)
+            return True
+        else:
+            return False
+        
+    def remove_song(self, song):
+        if song in self.songs:
+            self.songs.remove(song)
+            return True
+        else:
+            return False
+        
+    def view_playlist(self):
+        if self.songs:
+            return ' ' + ', '.join(self.songs)
+        else:
+            return 'Your playlist is empty.'
+        
+    def clear_playlist(self):
+        self.songs.clear()
+        length = len(self.songs)
+        if length == 0:
+            return 'Your playlist has been cleared.'
+        else:
+            return 'Clearing failed, or playlist was already empty'
--- frontend/src/components/ChatWidgetComponent.tsx
+++ frontend/src/components/ChatWidgetComponent.tsx
@@ -13,21 +13,21 @@ export const ChatWidgetComponent: React.FC = () => {
             "https://cdn.jsdelivr.net/npm/iaigroup-chatwidget@latest/build/bundle.min.js";
         script.async = true;
         script.onload = () => {
-            // @ts-ignore
+            //@ts-ignore
             ChatWidget({
-                name: "Chatbot",
-                serverUrl: "",
-                useFeedback: true,
-                useLogin: true,
+                name: "Music Recommendation Chat",
+                serverUrl: "http://127.0.0.1:5000",
+                useFeedback: false,
+                useLogin: false,
             });
 
             // Apply custom black text to widget
             const style = document.createElement("style");
             style.innerHTML = `
-        #chatWidgetContainer * {
-          color: black !important; /* Force all text to black */
-        }
-      `;
+            #chatWidgetContainer * {
+              color: black !important; /* Force all text to black */
+            }
+          `;
             document.head.appendChild(style);
         };
         document.body.appendChild(script);
